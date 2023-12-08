c9a2e391cb346822a974233d49ac403c2b8eca98
Suiji <1704802092@qq.com>
2023-12-08 19:39:30 +0800

    modified:   components/clock/index.scss 	modified:   components/search/children/NoResults.tsx 	new file:   components/search/index.scss 	modified:   components/search/index.tsx

--- components/search/children/NoResults.tsx
+++ components/search/children/NoResults.tsx
@@ -4,7 +4,7 @@ export const NoResults = () => {
 	const { indexUiState } = useInstantSearch()
 
 	return (
-		<div>
+		<div className="bg-white p-6">
 			<p>
 				No results for <q>{indexUiState.query}</q>.
 			</p>
--- components/search/index.tsx
+++ components/search/index.tsx
@@ -9,6 +9,8 @@ import {
 	NoResults,
 	NoResultsBoundary,
 } from './children'
+import './index.scss'
+import { useState } from 'react'
 
 const algoliaClient = algoliasearch(
 	'KH1UIFB7HU',
@@ -17,25 +19,53 @@ const algoliaClient = algoliasearch(
 
 const searchClient = {
 	...algoliaClient,
-	search(requests: any) {
+	search(requests) {
+		if (requests.every(({ params }) => !params?.query?.trim())) {
+			return Promise.resolve({
+				results: requests.map(() => ({
+					hits: [],
+					nbHits: 0,
+					nbPages: 0,
+					page: 0,
+					processingTimeMS: 0,
+					hitsPerPage: 0,
+					exhaustiveNbHits: false,
+					query: '',
+					params: '',
+				})),
+			})
+		}
 		return algoliaClient.search(requests)
 	},
 } as SearchClient
 
+// TODO: 添加缓存 - store map
 export const Search: React.FC = () => {
+	const [showResult, setShowResult] = useState(false)
+
 	return (
-		<InstantSearchNext
-			indexName="dev_NAME"
-			searchClient={searchClient}
-			routing
-			insights
-			future={{ preserveSharedStateOnUnmount: true }}>
-			<SearchBox autoFocus />
-			<EmptyQueryBoundary fallback={null}>
-				<NoResultsBoundary fallback={<NoResults />}>
-					<Hits hitComponent={Hit} />
-				</NoResultsBoundary>
-			</EmptyQueryBoundary>
-		</InstantSearchNext>
+		<div className="ais-search">
+			<InstantSearchNext
+				indexName="dev_NAME"
+				searchClient={searchClient}
+				routing
+				insights
+				future={{ preserveSharedStateOnUnmount: true }}>
+				<SearchBox
+					autoFocus
+					onFocus={() => setShowResult(true)}
+					onBlur={() => setShowResult(false)}
+				/>
+				{showResult && (
+					<div className="ais-result">
+						<EmptyQueryBoundary fallback={null}>
+							<NoResultsBoundary fallback={<NoResults />}>
+								<Hits hitComponent={Hit} />
+							</NoResultsBoundary>
+						</EmptyQueryBoundary>
+					</div>
+				)}
+			</InstantSearchNext>
+		</div>
 	)
 }
