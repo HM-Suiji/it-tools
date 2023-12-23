76084444c286ff18be74dfde6ce66f9fd0fb3736
Suiji <1704802092@qq.com>
2023-12-24 00:37:05 +0800

    modified:   utils/http.ts

--- utils/http.ts
+++ utils/http.ts
@@ -1,5 +1,17 @@
 const baseUrl = process.env.NEXT_PUBLIC_BASE_API_URL
 
-export const http = async (url: string, opt?: RequestInit) =>
-	(await fetch(baseUrl + url, { next: { revalidate: 1 }, ...opt })).json()
-//! 开发完去掉 { next: { revalidate: 1 }
+export const http = (() => {
+	if (process.env.NEXT_PUBLIC_ENV === 'DEV') {
+		return async (url: string, opt?: RequestInit) =>
+			(
+				await fetch(url.includes('http') ? url : baseUrl + url, {
+					next: { revalidate: 1 },
+					...opt,
+				})
+			).json()
+	}
+	return async (url: string, opt?: RequestInit) =>
+		(await fetch(url.startsWith('/') ? '/api' + url : url, opt)).json()
+})()
+
+console.log(http)
