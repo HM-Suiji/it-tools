f74fb2416bb8ec9262f128f4c8d6edacba950f9c
Suiji <1704802092@qq.com>
2024-01-29 22:44:50 +0800

    modified:   app/(tools)/layout.tsx

--- app/(tools)/layout.tsx
+++ app/(tools)/layout.tsx
@@ -5,7 +5,7 @@ import { CopyOutlined } from '@ant-design/icons'
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation'
 import Link from 'next/link'
-import { isMobile as _isMobile, http } from '@/utils'
+import { isMobile as _isMobile } from '@/utils'
 import { Calendar, Clock, Search, ToolTags } from '@/components'
 import './index.scss'
 
@@ -65,35 +65,47 @@ const Sider = ({ meta }: { meta?: Meta }) => {
 			collapsedWidth={0}>
 			<Clock />
 			<Calendar className="flex justify-center mt-8 mb-8 mx-auto" />
-			<div className="block p-4 bg-[#f0fff0] text-[#016c01] text-sm normal-shadow">
-				本工具数据均在本地浏览器处理，不会上传到网络服务器中， 请放心使用！
-			</div>
-			<div className="mt-8 mb-8 normal-shadow">
-				<Input
-					addonAfter={<CopyOutlined />}
-					value={window ? window.location.href : ''}
-					onClick={(e) => {
-						//TODO 后面得改
-						e.preventDefault()
-						e.currentTarget.select()
-						document.execCommand('copy')
-						e.currentTarget.blur()
-					}}
-				/>
-			</div>
-			<Card
-				id="card-tags"
-				title="工具标签"
-				className="normal-shadow mb-20 text-left"
-				bordered={false}>
-				<ToolTags tags={meta?.keywords?.split(',')} />
-			</Card>
+			<Widgets.Statement />
+			<Widgets.Link />
+			<Widgets.Tags meta={meta} />
 		</ASider>
 	)
 }
 
 const Container = ({ children }: { children: React.ReactNode }) => {
 	return <div className="pr-4 pl-4">{children}</div>
+}
+
+const Widgets = {
+	Statement: () => (
+		<div className="block p-4 bg-[#f0fff0] text-[#016c01] text-sm normal-shadow">
+			本工具数据均在本地浏览器处理，不会上传到网络服务器中， 请放心使用！
+		</div>
+	),
+	Link: () => (
+		<div className="mt-8 mb-8 normal-shadow">
+			<Input
+				addonAfter={<CopyOutlined />}
+				value={window ? window.location.href : ''}
+				onClick={(e) => {
+					//TODO 后面得改
+					e.preventDefault()
+					e.currentTarget.select()
+					document.execCommand('copy')
+					e.currentTarget.blur()
+				}}
+			/>
+		</div>
+	),
+	Tags: ({ meta }: { meta?: Meta }) => (
+		<Card
+			id="card-tags"
+			title="工具标签"
+			className="normal-shadow mb-20 text-left"
+			bordered={false}>
+			<ToolTags tags={meta?.keywords?.split(',')} />
+		</Card>
+	),
 }
 
 const ToolsLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
