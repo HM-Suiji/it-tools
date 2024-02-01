4505c62f12b0a4284066e7c72613322eac3d428f
Suiji <1704802092@qq.com>
2024-02-01 21:40:56 +0800

    modified:   app/login/index.scss 	modified:   app/login/page.tsx 	modified:   next.config.js 	modified:   package.json 	new file:   vitest.config.ts 	modified:   yarn.lock

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,33 @@
-import { Button } from 'antd'
+'use client'
+
+import { Button, Form } from 'antd'
 import Link from 'next/link'
 import Image from 'next/image'
+import { cookies } from 'next/headers'
 import LoginImg from '@/assets/img/login-bg.jpg'
 import './index.scss'
+
+type FieldType = {
+  username?: string
+  email?: string
+  password?: string
+}
+
+const onFinish = (values: any) => {
+  console.log('Success:', values)
+  localStorage.setItem('user-msg', values)
+}
+
+const onFinishFailed = (errorInfo: any) => {
+  console.log('Failed:', errorInfo)
+}
+
+const handleForget = () => {}
+
+// const getMemorize = () => {
+//   const userMsg = '{}'
+//   return userMsg ? JSON.parse(userMsg as string) : undefined
+// }
 
 const Login: React.FC = () => {
   return (
@@ -11,13 +36,53 @@ const Login: React.FC = () => {
         <Image className="absolute z-0" src={LoginImg} fill alt="" />
         <div className="flex flex-col absolute z-10 w-1/3 h-full bg-white right-0 justify-center items-center">
           <h1 className="text-4xl font-black">Login/Register</h1>
-          <input type="text" className="input" placeholder="USER NAME" />
-          <input type="text" className="input" placeholder="ACCOUNT" />
-          <input type="password" className="input" placeholder="PASSWORD" />
-          <Link href="#" className="float-right my-[10px]">
-            Forget the password?
-          </Link>
-          <Button className="g">Login</Button>
+          <Form
+            name="login-form"
+            // initialValues={getMemorize()}
+            className="mt-2"
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+          >
+            <Form.Item<FieldType>
+              name="username"
+              rules={[
+                { required: true, message: 'Please input your username!' },
+              ]}
+            >
+              <input type="text" className="input" placeholder="USER NAME" />
+            </Form.Item>
+            <Form.Item<FieldType>
+              name="email"
+              rules={[{ required: true, message: 'Please input your email!' }]}
+            >
+              <input type="text" className="input" placeholder="EMAIL" />
+            </Form.Item>
+            <Form.Item<FieldType>
+              className="mb-2"
+              name="password"
+              rules={[
+                { required: true, message: 'Please input your password!' },
+              ]}
+            >
+              <input type="password" className="input" placeholder="PASSWORD" />
+            </Form.Item>
+            <Link
+              href="#"
+              onClick={handleForget}
+              className="float-right my-2.5"
+            >
+              Forget the password?
+            </Link>
+            <Form.Item<FieldType> className="absolute bottom-10 m-5">
+              <Button
+                className="text-green-400 block w-[200px] h-[60px] text-4xl font-black rounded-[30px] bg-gradient-to-l from-[#9c88ff] to-[#3cadeb] text-center"
+                htmlType="submit"
+              >
+                Login
+              </Button>
+            </Form.Item>
+          </Form>
         </div>
       </div>
     </div>
