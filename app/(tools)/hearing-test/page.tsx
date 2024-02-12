fc7bd6925d70b158871198a314ee42faca27876f
Suiji <1704802092@qq.com>
2024-02-12 20:13:02 +0800

    modified:   app/(tools)/hearing-test/page.tsx

--- app/(tools)/hearing-test/page.tsx
+++ app/(tools)/hearing-test/page.tsx
@@ -7,85 +7,94 @@ const maxFrequency = 20000
 const step = 50
 
 interface OscillatorNodeObject {
-	oscillator: OscillatorNode | null
+  oscillator: OscillatorNode | null
 }
 
 const HearingTest: React.FC = () => {
-	const [isTesting, setIsTesting] = useState<boolean>(false)
-	const [currentFrequency, setCurrentFrequency] = useState<number>(0)
-	const oscillatorRef = useRef<OscillatorNodeObject>({ oscillator: null })
-	const timeoutIdRef = useRef<number | null>(null)
-	const audioContextRef = useRef<AudioContext | null>(null)
+  const [isTesting, setIsTesting] = useState<boolean>(false)
+  const [currentFrequency, setCurrentFrequency] = useState<number>(0)
+  const oscillatorRef = useRef<OscillatorNodeObject>({ oscillator: null })
+  const timeoutIdRef = useRef<number | null>(null)
+  const audioContextRef = useRef<AudioContext | null>(null)
 
-	useEffect(() => {
-		audioContextRef.current = new window.AudioContext()
-		return () => {
-			stopTest()
-		}
-	}, [])
+  const handleKeyDown = (e: KeyboardEvent) => {
+    if (e.key === ' ') {
+      e.preventDefault()
+      stopTest()
+    }
+  }
 
-	const playNextFrequency = (
-		frequency: number,
-		maxFrequency: number,
-		step: number
-	): void => {
-		const audioContext: AudioContext | null = audioContextRef.current
-		const oscillator: OscillatorNode | null = oscillatorRef.current.oscillator
+  useEffect(() => {
+    audioContextRef.current = new window.AudioContext()
+    document.addEventListener('keydown', handleKeyDown)
+    return () => {
+      stopTest()
+      document.removeEventListener('keydown', handleKeyDown)
+    }
+  }, [])
 
-		if (oscillator && audioContext) {
-			if (frequency <= maxFrequency) {
-				setCurrentFrequency(frequency)
-				oscillator.frequency.setValueAtTime(
-					frequency,
-					audioContext.currentTime + 0.05
-				)
-				timeoutIdRef.current = window.setTimeout(() => {
-					playNextFrequency(frequency + step, maxFrequency, step)
-				}, 100)
-			} else {
-				stopTest()
-			}
-		}
-	}
+  const playNextFrequency = (
+    frequency: number,
+    maxFrequency: number,
+    step: number,
+  ): void => {
+    const audioContext: AudioContext | null = audioContextRef.current
+    const oscillator: OscillatorNode | null = oscillatorRef.current.oscillator
 
-	const startTest = (): void => {
-		setIsTesting(true)
-		const audioContext: AudioContext | null = audioContextRef.current
-		const newOscillator: OscillatorNode = audioContext!.createOscillator()
-		const gainNode: GainNode = audioContext!.createGain()
+    if (oscillator && audioContext) {
+      if (frequency <= maxFrequency) {
+        setCurrentFrequency(frequency)
+        oscillator.frequency.setValueAtTime(
+          frequency,
+          audioContext.currentTime + 0.05,
+        )
+        timeoutIdRef.current = window.setTimeout(() => {
+          playNextFrequency(frequency + step, maxFrequency, step)
+        }, 100)
+      } else {
+        stopTest()
+      }
+    }
+  }
 
-		newOscillator.type = 'sine'
-		newOscillator.connect(gainNode)
-		gainNode.connect(audioContext!.destination)
-		newOscillator.start()
+  const startTest = (): void => {
+    setIsTesting(true)
+    const audioContext: AudioContext | null = audioContextRef.current
+    const newOscillator: OscillatorNode = audioContext!.createOscillator()
+    const gainNode: GainNode = audioContext!.createGain()
 
-		oscillatorRef.current.oscillator = newOscillator
-		playNextFrequency(minFrequency, maxFrequency, step)
-	}
+    newOscillator.type = 'sine'
+    newOscillator.connect(gainNode)
+    gainNode.connect(audioContext!.destination)
+    newOscillator.start()
 
-	const stopTest = (): void => {
-		setIsTesting(false)
-		clearTimeout(timeoutIdRef.current!)
-		const oscillator: OscillatorNode | null = oscillatorRef.current.oscillator
-		if (oscillator) {
-			oscillator.stop()
-			oscillator.disconnect()
-			// setCurrentFrequency(0)
-			oscillatorRef.current.oscillator = null
-		}
-	}
+    oscillatorRef.current.oscillator = newOscillator
+    playNextFrequency(minFrequency, maxFrequency, step)
+  }
 
-	return (
-		<div>
-			<button onClick={startTest} disabled={isTesting}>
-				开始测试听力范围
-			</button>
-			<button onClick={stopTest} disabled={!isTesting}>
-				暂停测试
-			</button>
-			<p>当前频率: {currentFrequency} Hz</p>
-		</div>
-	)
+  const stopTest = (): void => {
+    setIsTesting(false)
+    clearTimeout(timeoutIdRef.current!)
+    const oscillator: OscillatorNode | null = oscillatorRef.current.oscillator
+    if (oscillator) {
+      oscillator.stop()
+      oscillator.disconnect()
+      // setCurrentFrequency(0)
+      oscillatorRef.current.oscillator = null
+    }
+  }
+
+  return (
+    <div>
+      <button onClick={startTest} disabled={isTesting}>
+        开始测试听力范围
+      </button>
+      <button onClick={stopTest} disabled={!isTesting}>
+        暂停测试
+      </button>
+      <p>当前频率: {currentFrequency} Hz</p>
+    </div>
+  )
 }
 
 export default HearingTest
